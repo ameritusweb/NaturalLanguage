e8bbceb809bc7581717ceee1f0c3bfbbc8dd5408
Scott Seto <scott.seto@outlook.com>
2024-01-29 16:29:13 -0500

    Add logical rule.

---
+++ src/Logic/NorCombinedLogicalRule.cs
@@ -0,0 +1,28 @@
+﻿namespace NaturalLanguageProcess.Logic
+{
+    class NorCombinedLogicalRule : ILogicalRule
+    {
+        private List<ILogicalRule> Rules;
+
+        public NorCombinedLogicalRule()
+        {
+            Rules = new List<ILogicalRule>();
+        }
+
+        public void AddRule(ILogicalRule rule)
+        {
+            Rules.Add(rule);
+        }
+
+        public bool Evaluate(StoryContext context)
+        {
+            return Rules.All(rule => !rule.Evaluate(context));
+        }
+
+        public void Apply(StoryContext context)
+        {
+
+        }
+    }
+
+}
