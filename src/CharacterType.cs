1988f9f793a5f5f42b116cbbdc94b9bdc2aa69e7
Scott Seto <scott.seto@outlook.com>
2024-01-29 15:08:48 -0500

    Add enums.

---
+++ src/CharacterType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalLanguageProcess
+{
+    public enum CharacterType
+    {
+        None = 0,
+        Protagnist,
+        Antagonist,
+        ComicRelief,
+        LoveInterest,
+        Mentor,
+        Sidekick,
+        SupportingCharacter,
+        Other
+    }
+}
