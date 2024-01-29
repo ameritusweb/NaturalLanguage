be1f328af008c447a645acd0711f0e35a9db5e58
Scott Seto <scott.seto@outlook.com>
2024-01-29 15:49:30 -0500

    Add more enums.

---
+++ src/RelationshipPrepositionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalLanguageProcess
+{
+    public enum RelationshipPrepositionType
+    {
+        None = 0,
+        From,
+        For,
+        To,
+        AwayFrom,
+        By,
+        With,
+        Without
+    }
+}
