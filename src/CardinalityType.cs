a2be3e2a6823b4eac4e9d892cb276c1fc767bc79
Scott Seto <scott.seto@outlook.com>
2024-01-31 02:21:19 -0500

    Add cardinality.

---
+++ src/CardinalityType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalLanguageProcess
+{
+    public enum CardinalityType
+    {
+        None,
+        Singular, // Only one exists (e.g., a unique artifact)
+        Limited,  // Limited number exists (e.g., a few magical swords)
+        Plentiful, // Abundant, but not infinite (e.g., trees in a forest)
+        Infinite,  // Unlimited availability (e.g., stars in the sky)
+        Other // Cardinality is not defined or known
+    }
+}
