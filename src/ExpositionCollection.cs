f2c41746151878e214998e69835a4bcd6fef8b1d
Scott Seto <scott.seto@outlook.com>
2024-02-01 04:15:42 -0500

    Add exposition collection.

---
+++ src/ExpositionCollection.cs
@@ -0,0 +1,23 @@
+﻿using NaturalLanguageProcess.Sentences;
+
+namespace NaturalLanguageProcess
+{
+    public class ExpositionCollection
+    {
+        private Dictionary<ScenePurposeType, List<SentencePurpose>> expositions;
+
+        public ExpositionCollection()
+        {
+            expositions = new Dictionary<ScenePurposeType, List<SentencePurpose>>();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var scenePurpose1 = ScenePurposeType.RescuePrincessFromTower;
+            SentencePurpose shareAnExperience = new SentencePurpose(SentencePurposeType.ShareAnExperience);
+            shareAnExperience.Sentences.Add(new SubjectVerbObject());
+            shareAnExperience.Sentences.Add(new SubjectVerbObject());
+        }
+    }
+}
---
+++ src/Scene.cs
@@ -0,0 +1,37 @@
+﻿namespace NaturalLanguageProcess
+{
+    public class Scene
+    {
+        public StoryWord ObjectYouAreLookingFor { get; set; }
+
+        public StoryWord TypeOfMission { get; set; }
+
+        public StoryWord ImportanceLevelOfMission { get; set; }
+
+        public StoryWord TimeOfDay { get; set; }
+
+        public StoryWord InitialAssessmentOfMission { get; set; }
+
+        public StoryWord AnIndicatorThatTheMissionStarted { get; set; }
+
+        public StoryWord StatusOfTheMission { get; set; }
+
+        public StoryWord TheGoalOfTheMission { get; set; }
+
+        public StoryWord ASuccessFactorOfTheMission { get; set; }
+
+        public StoryWord MissionComparison { get; set; }
+
+        public StoryWord AnObstacleToTheMission { get; set; }
+
+        public StoryWord TheIdealFutureStateOfTheMission { get; set; }
+
+        public StoryWord MissionProgress { get; set; }
+
+        public StoryWord TheIdealMentalStateForTheMission { get; set; }
+
+        public StoryWord TheIdealStrategyForTheMission { get; set; }
+
+        public StoryWord LocationOfTheVictim { get; set; }
+    }
+}
