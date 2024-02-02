4ac6d1ad52177920a8f3352f1a8a137c3ae594aa
Scott Seto <scott.seto@outlook.com>
2024-02-01 21:33:42 -0500

    Update scene class.

--- src/Scene.cs
+++ src/Scene.cs
@@ -4,11 +4,21 @@
     {
         public StoryWord ObjectYouAreLookingFor { get; set; }
 
+        public StoryWord ObjectThatAssistsTheMission { get; set; }
+
         public StoryWord TypeOfMission { get; set; }
 
         public StoryWord ImportanceLevelOfMission { get; set; }
 
         public StoryWord TimeOfDay { get; set; }
+
+        public StoryWord APlaceToCommunicate { get; set; }
+
+        public StoryWord TheMainCauseOfTheProblem { get; set; }
+
+        public StoryWord TheStatusOfTheMainCauseOfTheProblem { get; set; }
+
+        public StoryWord SpectatorToOrAudienceOfTheMission { get; set; }
 
         public StoryWord InitialAssessmentOfMission { get; set; }
 
@@ -18,19 +28,37 @@
 
         public StoryWord TheGoalOfTheMission { get; set; }
 
+        public StoryWord APlanToSucceedAtMission { get; set; }
+
+        public StoryWord TheImmediateEffectsOfTheProblem { get; set; }
+
         public StoryWord ASuccessFactorOfTheMission { get; set; }
 
         public StoryWord MissionComparison { get; set; }
 
         public StoryWord AnObstacleToTheMission { get; set; }
 
+        public StoryWord StatusOfTheObstacleToTheMission { get; set; }
+
         public StoryWord TheIdealFutureStateOfTheMission { get; set; }
 
         public StoryWord MissionProgress { get; set; }
 
+        public StoryWord MissionOriginator { get; set; }
+
+        public StoryWord LengthOfTimeThePotentialSolutionWillLast { get; set; }
+
+        public StoryWord TheTimeLeftToCompleteTheMission { get; set; }
+
+        public StoryWord PossibleSolutionToConsider { get; set; }
+
         public StoryWord TheIdealMentalStateForTheMission { get; set; }
 
+        public StoryWord TheStateOfTheObjectThatAssistsTheMission { get; set; }
+
         public StoryWord TheIdealStrategyForTheMission { get; set; }
+
+        public StoryWord LocationOfTheMission { get; set; }
 
         public StoryWord LocationOfTheVictim { get; set; }
     }
